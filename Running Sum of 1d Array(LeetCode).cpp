04f903aa9064a1961d1d2b22a179c5018f591b39
DEO-DECODE <106802468+DEO-DECODE@users.noreply.github.com>
2023-02-18 19:42:20 +0530

    Create Running Sum of 1d Array(LeetCode).cpp

---
+++ Running Sum of 1d Array(LeetCode).cpp
@@ -0,0 +1,13 @@
+#include <bits/stdc++.h>
+using namespace std;
+//Running Sum of 1d Array(LeetCode)
+vector<int> runningSum(vector<int> &nums)
+{
+    vector<int> ans(nums.size());
+    ans[0] = nums[0];
+    for (int i = 1; i < nums.size(); ++i)
+    {
+        ans[i] = nums[i] + ans[i - 1];
+    }
+    return ans;
+}
