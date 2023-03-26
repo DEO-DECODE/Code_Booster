bc24b831bc47363c8d929d07060f15e1d80da821
DEO-DECODE <106802468+DEO-DECODE@users.noreply.github.com>
2023-03-26 16:49:27 +0530

    Update Gas Station (leetCode).cpp

--- Gas Station (leetCode).cpp
+++ Gas Station (leetCode).cpp
@@ -3,25 +3,15 @@ using namespace std;
 // Gas Station (leetCode)
 int canCompleteCircuit(vector<int> &gas, vector<int> &cost)
 {
+   int possibleAnswer=-1, stock=0;
     if(accumulate(gas.begin(), gas.end(),0)-accumulate(cost.begin(), cost.end(),0)<0) return -1;
-    int n=gas.size();
-    queue<int> q;
-    q.push(0);
-    int netPetrol = 0;
-    while (q.size() < n)
-    {
-        int pp = q.back();
-        netPetrol += (gas[pp] - cost[pp]);
-        if (netPetrol < 0)
-        {
-            while (q.size() > 0)
-            {
-                q.pop();
-            }
+    for(int i=0; i<gas.size(); ++i){
+        stock+=gas[i];
+        stock-=cost[i];
+        if(stock<0){
+            stock=0;
+            possibleAnswer=i+1;
         }
-        if (q.empty())
-            netPetrol = 0;
-        q.push((pp + 1) % n);
     }
-    return q.front();
+    return possibleAnswer;
 }
